8cbce0042f40d4ece8a3736c6d585b2b07ce6f84
osvaldokalvaitir <osvaldokalvaitir@outlook.com>
2019-01-10 20:21:24 -0200

    Adicionando favoritos

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,5 +1,13 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest } from 'redux-saga/effects';
+
+import { addFavorite } from './favorites';
+
+/**
+ * takeLatest -> se tiver várias requisições, ele cancela a que está acontecendo
+ *               e vai pegar sempre a última requisição
+ * takeEvery  -> se tiver várias requisições, ele faz todas em background
+ */
 
 export default function* rootSaga() {
-  yield all([]);
+  yield all([takeLatest('ADD_FAVORITE_REQUEST', addFavorite)]);
 }
